0336521a31565e77743698215947e53836fa6eba
Soma Osvay <osvays@starschema.net>
2024-01-10 02:22:11 +0100

    c# code prettified

--- dotnet/Program.cs
+++ dotnet/Program.cs
@@ -1,5 +1,26 @@
-﻿using System;
-using System.Runtime.InteropServices;
+﻿// Dive right in -> we use top level statements, no need for namespace, class, main method, nothing. Imports are done globally in .csproj for clean code.
+
+// create the iterator (calls rust)
+IntPtr iterator = create_arrow_iterator();
+IntPtr samplePtr;
+// call iterator.next() (in rust) until we get a zero pointer indicating that we finished
+while ((samplePtr = next_arrow(iterator)) != IntPtr.Zero) {
+    // unmarshal the pointer returned from rust into the structure
+    ArrowSample sample = Marshal.PtrToStructure<ArrowSample>(samplePtr);
+    Console.WriteLine($"ArrowSample: [[{sample.data[0]}, {sample.data[1]}], [{sample.data[2]}, {sample.data[3]}]]");
+}
+// free the rust iterator
+free_arrow_iterator(iterator);
+
+
+[DllImport("rust_test.dylib")]
+static extern IntPtr create_arrow_iterator();
+
+[DllImport("rust_test.dylib")]
+static extern IntPtr next_arrow(IntPtr iterator);
+
+[DllImport("rust_test.dylib")]
+static extern void free_arrow_iterator(IntPtr iterator);
 
 [StructLayout(LayoutKind.Sequential)]
 public struct ArrowSample {
@@ -7,24 +28,9 @@ public struct ArrowSample {
     public int[] data;
 }
 
-class Program {
-    [DllImport("rust_test.dylib")]
-    public static extern IntPtr create_arrow_iterator();
 
-    [DllImport("rust_test.dylib")]
-    public static extern IntPtr next_arrow(IntPtr iterator);
 
-    [DllImport("rust_test.dylib")]
-    public static extern void free_arrow_iterator(IntPtr iterator);
 
-    static void Main() {
-        IntPtr iterator = create_arrow_iterator();
-        IntPtr samplePtr;
-        while ((samplePtr = next_arrow(iterator)) != IntPtr.Zero) {
-            ArrowSample sample = Marshal.PtrToStructure<ArrowSample>(samplePtr);
-            Console.WriteLine($"ArrowSample: [[{sample.data[0]}, {sample.data[1]}], [{sample.data[2]}, {sample.data[3]}]]");
-        }
 
-        free_arrow_iterator(iterator);
-    }
-}
+
+
